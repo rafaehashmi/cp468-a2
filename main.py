202b15597b11cc240b735a4b548dcc053ef803af
KomalAhmed1 <ahme7773@mylaurier.ca>
2026-07-19 21:40:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from agents import RandomAgent, RuleBasedAgent, MinimaxAgent
 MASTER_SEED = 42
 GAMES_PER_PAIRING = 30
 
-SHOW_BOARD = True
+SHOW_BOARD = False
 
 
 # =====================================================
